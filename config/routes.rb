849a0fcff694a81ba414e2130128875f62a596f0
daido1976 <daidoshota@daidoushoutanoMacBook-Air.local>
2018-02-16 07:34:43 +0900

    users index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root 'users#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
