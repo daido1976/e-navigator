e2f80643c58ecf712e2430cf74730dd3c06cb84e
daido1976 <daidoshota@daidoushoutanoMacBook-Air.local>
2018-02-17 14:07:59 +0900

    view modification

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  enum gender: { male: 0, female: 1 }
+  enum gender: { 男性: 0, 女性: 1 }
+
+  def age
+    date_format = "%Y%m%d"
+    (Date.today.strftime(date_format).to_i - birthday.strftime(date_format).to_i) / 10000
+  end
 end
