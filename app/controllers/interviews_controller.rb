b5ad8f332dbea78619363de755e464fc4b5bfe25
daido1976 <daidoshota@daidoushoutanoMacBook-Air.local>
2018-02-21 02:31:45 +0900

    Interviews index

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,5 +1,7 @@
 class InterviewsController < ApplicationController
   def index
+    @user = User.find(params[:user_id])
+    @interviews = @user.interviews.order(id: :desc)
   end
 
   def new
--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,3 +1,4 @@
 class Interview < ApplicationRecord
   belongs_to :user
+  enum judgement: { pend: 0, approval: 1, rejection: 2 }
 end
