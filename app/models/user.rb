be007218f52391b1b9e2d7685cb83cba74ee6d5f
daido1976 <daidoshota@daidoushoutanoMacBook-Air.local>
2018-02-20 14:33:44 +0900

    user interviews association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :interviews
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   enum gender: { 男性: 0, 女性: 1 }
