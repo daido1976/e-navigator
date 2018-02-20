85f109e45bc7596eac372e0c009521c7e5ff9ba4
daido1976 <daidoshota@daidoushoutanoMacBook-Air.local>
2018-02-21 01:39:31 +0900

    Rails db:migrate

--- db/migrate/20180220052149_create_interviews.rb
+++ db/migrate/20180220052149_create_interviews.rb
@@ -1,7 +1,9 @@
 class CreateInterviews < ActiveRecord::Migration[5.1]
   def change
     create_table :interviews do |t|
-
+      t.date :interview_date
+      t.integer :judgement
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
