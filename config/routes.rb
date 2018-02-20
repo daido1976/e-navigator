204f0a415cfd980e3dc00bfcfc1d62c2a283c7ec
daido1976 <daidoshota@daidoushoutanoMacBook-Air.local>
2018-02-21 01:44:36 +0900

    Routing&rails g controller interviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'interviews/index'
+
+  get 'interviews/new'
+
   devise_for :users
   devise_scope :user do
     authenticated :user do
@@ -7,5 +11,8 @@ Rails.application.routes.draw do
     unauthenticated :user do
       root 'devise/sessions#new', as: :unauthenticated_root
     end
+  end
+  resources :users do
+    resources :interviews
   end
 end
