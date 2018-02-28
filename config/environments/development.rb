1debbd12de4623e5f4659cba46a41771b025265f
daido1976 <daidoshota@daidoushoutanoMacBook-Air.local>
2018-03-01 08:54:05 +0900

    add Dotenv to development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,5 +52,17 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.delivery_method = :letter_opener_web
+  Bundler.require(*Rails.groups)
+  Dotenv::Railtie.load
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'smtp.gmail.com',
+    user_name:            ENV['LOGIN_NAME'],
+    password:             ENV['LOGIN_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
